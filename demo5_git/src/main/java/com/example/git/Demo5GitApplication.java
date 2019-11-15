c0c108934b1415f858eba98b987eca0c1bda199c
yu <319931981@qq.com>
2019-11-15 09:06:02 +0800

    这就是一个测试呀

---
+++ demo5_git/src/main/java/com/example/git/Demo5GitApplication.java
@@ -0,0 +1,13 @@
+package com.example.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo5GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo5GitApplication.class, args);
+    }
+
+}
---
+++ demo5_git/src/main/java/com/example/test/Test.java
@@ -0,0 +1,8 @@
+package com.example.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是测试");
+        System.out.println("这不是演习");
+    }
+}
